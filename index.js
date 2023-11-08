5e0ed56ba4a844fa3d34d09c08994514a0577926
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-11-08 23:08:41 +0600

    code updated

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         // Database and Collection 
         const jobsCollections = client.db('jobs').collection('all_jobs');
